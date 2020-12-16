d5abeb0f1e7e1cc156b62a45894bb094d832efea
peggy1008123 <peggy1008123@yahoo.com.tw>
2020-12-17 01:14:22 +0800

    hambergur

--- js/main.js
+++ js/main.js
@@ -4,39 +4,38 @@ $('.freegame_item').slick({
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+    responsive: [{
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 320,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
     ]
-  });
-  $(document).ready(function(){
+});
+$(document).ready(function() {
     let $ul = $(".head_nav"),
-    $menu = $(".nav_menu");
-    $($menu).click(function(){
-      $ul.toggleClass("menu_click");
+        $menu = $(".nav_menu");
+    $($menu).click(function() {
+        $ul.toggleClass("menu_click");
     });
-  });
+});
