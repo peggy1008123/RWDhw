dfda62bbdfe95484a4bc8743eb16e3662bb38146
peggy1008123 <peggy1008123@yahoo.com.tw>
2020-12-16 17:49:15 +0800

    login  button out

--- js/main.js
+++ js/main.js
@@ -33,3 +33,12 @@ $('.freegame_item').slick({
       // instead of a settings object
     ]
   });
+  $(document).ready(function () {
+    $('.burgar_btn').on('click', function (e) {
+        // 用 on 監聽 .burgar_btn 的點擊功能
+        e.preventDefault();
+        // 點擊 burgar_btn 時，讓原本的 a 連結效果無效
+        $('body').toggleClass('burgar_btn-show');
+        // 讓瀏覽器知道有點擊 .burgar_btn 的時候，會有收合功能，並在 body 產生一個新的 Class 名稱 burgar_btn-show
+    });
+});
